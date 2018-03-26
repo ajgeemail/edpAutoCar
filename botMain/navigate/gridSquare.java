c85d312535f6322d81d7d3bb674518e8817109cd
Brady Walker <brady.walker96@gmail.com>
2018-03-26 16:08:25 +1100

    Added navigate folder

---
+++ botMain/navigate/gridSquare.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class gridSquare here.
+ *
+ * @author Brady Walker
+ * @version 1.0
+ */
+public class gridSquare
+{
+    public int value;
+    public int xPos;
+    public int yPos;
+
+    /**
+     * Constructor for objects of class gridSquare
+     */
+    public gridSquare()
+    {
+        value = -1;
+        xPos = -1;
+        yPos = -1;
+    }
+}
